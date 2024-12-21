f6b6e1309843e807d6917ec65daa6c1a64561a2a
AbdurRafay-coder <aeoliansound93@gmail.com>
2024-12-21 16:11:40 +0500

    added logbook and more DSA concepts

--- Base_system.cpp
+++ Base_system.cpp
@@ -3,11 +3,74 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <iomanip>
 using namespace std;
 //ranking will done using BST
 //AVL tree for balanced BST
 //File handling for users data
 //regx B1hterb0l!
+
+class Queue
+{
+    public:
+    struct Node
+    {
+        int data;
+        Node* next;
+    };
+    Node* listhead = NULL;
+    void push(int val);
+    void pop();
+    void display();
+    int front();
+};
+
+void Queue::push(int val)
+{
+    Node* newnode = new Node();
+    newnode->data = val;
+    if(listhead == NULL)
+    {
+        listhead = newnode;
+        listhead->next = NULL;
+    }
+    else
+    {
+        Node* temp = listhead;
+         while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newnode;
+        newnode->next = NULL;
+    }
+}
+
+void Queue::pop()
+{
+    Node* temp = listhead;
+    listhead = listhead->next;
+    delete temp;
+}
+
+void Queue::display()
+{
+    Node* temp = listhead;
+    while(temp != NULL)
+    {
+        cout<<"UserID: "<<temp->data<<" has logged in"<<endl;
+        temp = temp->next;
+    }
+}
+
+int Queue::front()
+{
+    if(listhead != NULL)
+    {return listhead->data;}
+    else{
+        cout<<"EMPTY"<<endl;
+    }
+}
 
 class User
 {
@@ -19,7 +82,9 @@ class User
     friend void add_to_file(vector<User>& users,const string& filename);
     friend void add_user(vector<User>& users);
     friend void load_users(vector<User>& users,const string& filename);
-    friend void login(vector<User>& users);
+    friend void login(vector<User>& users, Queue& q);
+    friend void logout(User& users);
+    friend void codeloop(User& user);
 };
 int User::ID = 0;
 
@@ -99,7 +164,7 @@ void load_users(vector<User>& users,const string& filename)
     }
 }
 
-void login(vector<User>& users)
+void login(vector<User>& users, Queue& q)
 {
     User temp;
     cout<<"Please enter your username: ";
@@ -116,6 +181,7 @@ void login(vector<User>& users)
                 {
                     cout<<"You are now logged in"<<endl;
                     u.status = true;
+                    q.push(u.ID);
                     return;
                 }
                 else if(temp.password == "0")
@@ -134,11 +200,15 @@ void login(vector<User>& users)
     }
 }
 
-
+void codeloop(User& user)
+{
+    
+}
 
 int main()
 {
     vector<User> users;
+    Queue logbook;
     fstream MyFile("userdata.txt", ios::in | ios::out | ios::app);
     int choice = 0;
     cout<<"Please enter the number corresponding to the action you want to perform"<<endl;
@@ -155,7 +225,7 @@ int main()
         }
         if(choice == 2)
         {
-
+            login(users,logbook);
         }
 
     }
